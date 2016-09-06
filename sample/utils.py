f30ab35b68e26055fa860a3ed036d0fe48dda99c
Sudhanshu Mittal <sudhanshumittal1992@gmail.com>
2016-09-06 19:35:42 +0530

    changed 2d plot function to accept multiple graphs data

--- sample/utils.py
+++ sample/utils.py
@@ -12,44 +12,49 @@ class DefaultValueDict(dict):
     def __missing__(self, key):
         return 0.
 
-def epsilon_greedy_action(self, epsilon, action_values):
+def epsilon_greedy_action(epsilon, action_values):
     action = None
     exploring = random() < epsilon
     if exploring:
-        action = randint(0, len(self.action_space) - 1)
+        action = randint(0, len(action_values) - 1)
     else:
         action = argmax(action_values)
     return action
 
 def plot(player):
-    fig = plt.figure()
-    ax = Axes3D(fig)
     x = player.dealer_start
     y = player.player_states
     X, Y = meshgrid(x, y)
     Z = zeros((21, 10))
     state_space = product(*[player.dealer_start, player.player_states])
     for state in state_space:
-        policy = max([player.Q[state+ (action,)] for action in player.action_space])
-        Z[state[1]-1][state[0]-1] = policy
+        policy = max([player.Q[state + (action,)] for action in player.action_space])
+        Z[state[1] - 1][state[0] - 1] = policy
+    plot3d(X, Y, Z)
+
+def plot3d(X, Y, Z):
+    fig = plt.figure()
+    ax = Axes3D(fig)
     ax.plot_wireframe(X, Y, Z)
     plt.show()
-    
-def plot2d(ip_dict):
-    assert(isinstance(ip_dict, dict))
-    keys = ip_dict.keys()
-    values = ip_dict.values()
-    plt.scatter(keys, values)
+        
+def plot2d(x=[], y=[]):
+    assert(isinstance(x, list)), "input x should be a list"
+    assert(isinstance(y, list)), "input y should be a list"
+    for x_, y_ in zip(x, y):
+        plt.plot(x_, y_)
     plt.show()
     
 def calculate_MSE(Q1, Q2):
     dealer_start = range(1, 11)
     player_states = range(1, 22)
     state_space = product(*[dealer_start, player_states])
-    MSE = 0.
+    total_sq_err = 0.
+    actions = [0,1]
     for state in state_space:
-        for action in [0,1]:
+        for action in [0, 1]:
             key = state + (action,)
-            sqErr = pow(Q1[key] - Q2[key],2)
-            MSE +=sqErr
+            sq_err = pow(Q1[key] - Q2[key], 2)
+            total_sq_err += sq_err
+    MSE = total_sq_err/ (len(dealer_start)*len(player_states)*len(actions))
     return MSE
